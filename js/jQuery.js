fadd285511331af895b93daa9ae42fdfd20ee629
Gary Luypaert <g.luypaert@me.com>
2017-06-15 13:18:13 +0200

    Ajout by Gary

--- js/jQuery.js
+++ js/jQuery.js
@@ -2,8 +2,12 @@ $(document).ready(function() {
 
   $(".moreMoviesBtn").click(function() {
     $(".moreMoviesBox").slideToggle("slow")
-//    $(".moreMoviesBtn").text("Moins de films");
+    $(".moreMoviesBtn").text("Moins de films");
+  });
 
+  $(".moreSeriesBtn").click(function() {
+    $(".moreSeriesBox").slideToggle("slow")
+//    $(".moreSeriesBtn").text("Moins de films");
   });
 
 });
