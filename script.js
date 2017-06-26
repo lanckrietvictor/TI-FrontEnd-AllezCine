739a92b130aee4cdcbb679f2209fe544a2ecffd7
lanckrietvictor <lanckrietvictor@gmail.com>
2017-06-26 16:33:55 +0200

    start newsletter

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
 /*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   Vérifier l'age
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
 
-function verifAge() {
-  var age = document.getElementById("inputAge").value;
-  var modalAge = document.getElementById("myModal");
+  function verifAge() {
+  	var age = document.getElementById("inputAge").value;
+  	var modalAge = document.getElementById("myModal");
 
-  if (age < 18) {
-    alert("Hop hop hop! Redirection en cours!");
-    window.location.assign("site2.html");
+  	if (age < 18) {
+  		alert("Hop hop hop! Redirection en cours!");
+  		window.location.assign("site2.html");
+  	}
+  	else {
+  		alert("Welcome");
+  		window.location.assign("enter.html");
+  	}
+
   }
-  else {
-    alert("Welcome");
-    window.location.assign("enter.html");
-  }
-
-}
 
 
 
 /*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   Back To The Top Arrow
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
 
-		window.onload = function () {
-			buttonCreation();
-			document.getElementById("myBtn").addEventListener("click", topFunction);
-			document.getElementById("myBtn").setAttribute("style", "display: none; position: fixed; bottom: 20px; right: 30px; z-index: 99;	border: none; outline: none;	background-color: #FF8D1B; color: white; cursor: pointer; padding: 15px;			border-radius: 50%;");
-		};
+  window.onload = function () {
+  	buttonCreation();
+  	document.getElementById("myBtn").addEventListener("click", topFunction);
+  	document.getElementById("myBtn").setAttribute("style", "display: none; position: fixed; bottom: 20px; right: 30px; z-index: 99;	border: none; outline: none;	background-color: #FF8D1B; color: white; cursor: pointer; padding: 15px;			border-radius: 50%;");
+  };
 
-		function buttonCreation() {
-			var btn = document.createElement("BUTTON");
-			btn.setAttribute("id", "myBtn");
-				var icon = document.createElement("span");
-				icon.className = "glyphicon glyphicon-arrow-up";
+  function buttonCreation() {
+  	var btn = document.createElement("BUTTON");
+  	btn.setAttribute("id", "myBtn");
+  	var icon = document.createElement("span");
+  	icon.className = "glyphicon glyphicon-arrow-up";
 			//document.getElementById("myBtn").innerHTML = icon
 			btn.appendChild(icon);
 			document.body.appendChild(btn);
@@ -59,24 +59,31 @@ function verifAge() {
 
 /*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   Cookies
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
 
-window.addEventListener("load", function(){
-window.cookieconsent.initialise({
-  "palette": {
-    "popup": {
-      "background": "#eaf7f7",
-      "text": "#5c7291"
-    },
-    "button": {
-      "background": "#56cbdb",
-      "text": "#ffffff"
-    }
-  },
-  "content": {
-    "message": "This site uses cookies. By continuing to browse the site, you are agreeing to our use of cookies. Find out more.",
-    "dismiss": "Got it!",
-    "link": "Learn More"
-  }
-})
-});
+  window.addEventListener("load", function(){
+  	window.cookieconsent.initialise({
+  		"palette": {
+  			"popup": {
+  				"background": "#eaf7f7",
+  				"text": "#5c7291"
+  			},
+  			"button": {
+  				"background": "#56cbdb",
+  				"text": "#ffffff"
+  			}
+  		},
+  		"content": {
+  			"message": "This site uses cookies. By continuing to browse the site, you are agreeing to our use of cookies. Find out more.",
+  			"dismiss": "Got it!",
+  			"link": "Learn More"
+  		}
+  	})
+  });
+
+
+/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  Slider shop
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+
+  
