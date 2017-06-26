3e86d3be9418483a9870a982dc2ab93f81a7e69e
Gary Luypaert <g.luypaert@me.com>
2017-06-26 09:52:00 +0200

    ajout files

--- js/jQuery.js
+++ js/jQuery.js
@@ -1,13 +1,42 @@
 $(document).ready(function() {
 
+  $(window).on('load',function(){
+      $('#myModalAge').modal('show');
+  });
+
+
+  $(".categories-movie-series").mouseenter(function() {
+    $(this).css("background-color", "#FF8D1B");
+  });
+  $(".categories-movie-series").mouseleave(function() {
+    $(this).css("background-color", "#E9E9E9");
+  });
+  $(".categories-movie-series").click(function() {
+    $(this).css("background-color", "#FF8D1B");
+  });
+
+  $("#actions-btn-movie").click(function() {
+    $("#row-movie-delete").removeAttr("class");
+  });
+
   $(".moreMoviesBtn").click(function() {
-    $(".moreMoviesBox").slideToggle("slow")
-    $(".moreMoviesBtn").text("Moins de films");
+    $(".moreMoviesBox").slideToggle("slow");
+    $(".moreMoviesBtn").hide();
+  });
+
+  $(".lessMoviesBtn").click(function() {
+    $(".moreMoviesBox").slideToggle("slow");
+    $(".moreMoviesBtn").show();
   });
 
   $(".moreSeriesBtn").click(function() {
-    $(".moreSeriesBox").slideToggle("slow")
-//    $(".moreSeriesBtn").text("Moins de films");
+    $(".moreSeriesBox").slideToggle("slow");
+    $(".moreSeriesBtn").hide();
+  });
+
+  $(".lessSeriesBtn").click(function() {
+    $(".moreSeriesBox").slideToggle("slow");
+    $(".moreSeriesBtn").show();
   });
 
 });
